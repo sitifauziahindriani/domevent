2bc192b03430133a4f3624a4b15b24fa4b625626
sitifauziahindriani <fauziahsiti1410@gmail.com>
2020-09-30 08:43:52 +0700

    upload domevent

---
+++ main.js
@@ -0,0 +1,27 @@
+// kita buat sebuah fungsi yang akan memunculkan modal
+const showModal = () => {
+
+    //kita ambil elmen yg akan di SHOW / tampilkan
+    let modal = document.querySelector('.modalMenu')
+    // kita atur style jadi dari elment tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'flex'
+
+}
+
+// fungsi untuk hide elment
+const hideModal = () => {
+    let modal = document.querySelector('.modalMenu')
+    // kita atur style jadi dari elment tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'none'
+
+
+}
+
+const hideaboutAlert = () => {
+    let aboutAlert = document.querySelector('.aboutAlert')
+    aboutAlert.style.display = 'none'
+}
+
+
